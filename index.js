b89e7db00f344a95ea8adb81bd601df8303d1c86
Lucas Coutinho <lucas.coutinho@eagronom.com>
2022-05-13 11:27:18 -0300

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 mongoose.connect(`mongodb+srv://${process.env.LOGIN}:${process.env.PASSOWRD}@cluster0.f1yex.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
 .then(() => {
     console.log('Conectado')
-    app.listen(3000)
+    app.listen(port)
 })
 .catch(err => {
     console.log(err)
